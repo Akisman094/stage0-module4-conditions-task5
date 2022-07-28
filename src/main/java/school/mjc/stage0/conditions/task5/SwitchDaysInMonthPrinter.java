cf6e2a70a3325c8825c5a3da05d9ffefe8a87eeb
Akmal Ismoilov <master80098@gmail.com>
2022-07-28 14:30:29 +0500

    Tasks completed!

--- src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/SwitchDaysInMonthPrinter.java
@@ -1,6 +1,19 @@
 package school.mjc.stage0.conditions.task5;
 
+import java.time.YearMonth;
+
 public class SwitchDaysInMonthPrinter {
     public void amountOfDays(int month) {
+        int numOfDays;
+        switch(month)
+        {
+            case 1,2,3,4,5,6,7,8,9,10,11,12:
+                numOfDays= YearMonth.of(2022, month).lengthOfMonth();
+                System.out.println(numOfDays);
+                break;
+            default:
+                System.out.println("wrong number!"); //lol
+        }
+
     }
 }
